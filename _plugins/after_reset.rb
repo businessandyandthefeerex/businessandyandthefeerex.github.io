cd200440a54cbba01231ff7c6ca4e69e13adfc98
businessandyandthefeerex <businessandyandthefeerex@gmail.com>
2025-04-25 23:33:21 +1200

    moving csv generation to the pre-commit hook and settin g up a versioned .githooks folder

--- _plugins/after_reset.rb
+++ _plugins/after_reset.rb
@@ -6,7 +6,7 @@ Jekyll::Hooks.register :site, :after_reset do |site|
     system("ruby #{File.join(Dir.pwd, 'assets', 'scripts', 'generate_region_pages.rb')}")
     system("ruby #{File.join(Dir.pwd, 'assets', 'scripts', 'generate_review_date_hierarchy.rb')}")
     system("ruby #{File.join(Dir.pwd, 'assets', 'scripts', 'generate_collections.rb')}")
-    system("ruby #{File.join(Dir.pwd, 'assets', 'scripts', 'generate_map_export.rb')}")
+    # system("ruby #{File.join(Dir.pwd, 'assets', 'scripts', 'generate_map_export.rb')}") # this has been moved to .git\hooks\pre-commit so that the file is alwayws up to date
   
     puts "Pre-build tasks completed."
   end
