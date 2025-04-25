3f3a867a954d54074b4f46c36f3bb57e0ac8e044
businessandyandthefeerex <businessandyandthefeerex@gmail.com>
2025-04-25 23:45:27 +1200

    minor changes to pre-commit script

--- .githooks/pre-commit.sh
+++ .githooks/pre-commit.sh
@@ -5,9 +5,10 @@ ruby ./assets/scripts/generate_map_export.rb
 
 # Exit with status code 0 to proceed with the commit, or non-zero to abort
 if [ $? -ne 0 ]; then
-  echo "The script failed. Commit aborted."
+  echo "The csv generator script failed. Commit aborted."
   exit 1
 fi
 
 # Allow the commit to proceed
+echo "The csv generator script ran."
 exit 0
